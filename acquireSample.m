3d47d791ad18dd57b919ee320e632b317517f711
Ryan <NeuroRYoung@gmail.com>
2015-08-06 02:41:53 -0400

    First tracked commmit, in this repository. See MazeAnalysis repository for history of development for these three files up to the point where I decided to give them a repo.

---
+++ acquireSample.m
@@ -0,0 +1,192 @@
+function [times] = acquireSample(data, sampleParams)
+% acquireSample
+% 
+% This function will be used to simplify the process of selecting
+% particular data from the complete trajcetories of animals.
+%
+% INPUTS
+% -----------------------
+%
+% data, contains general data from which we might want to generate a sample
+% of times from
+%
+%	data.linpos		... all linearized position data
+%	data.pos		... all raw position data
+%	data.trajinfo	... 
+%
+% sampleParams ...
+%	struct that can contain a variety of data types. For starters,
+%	I've added three selection criteria, though I expect the function's
+%	selection criteria to increase over time.
+%   
+%   FIELDS - each field is OPTIONAL, and acts to further constrain the
+%   sampled times
+%
+%   'sampleParams.trajbound_type' = 0 or 1 .. if 0, filter times that are
+%   outboud. If 1, filter times that are inbound.
+%
+%   'sampleParams.circleParams' .. structure that contains data for
+%   filtering times circumscribed by a position.
+%
+%       'sampleParams.circleParams.radius' .. the radius in pixels to
+%       detect around the point.
+%
+%       'sampleParams.circleParams.center' .. specifies [x y] to sample
+%       around. Alternatively, one can specify the segment and the method
+%       will find the [x y] by filling the .segmentCoordinate field below.
+%
+%       'sampleParams.circleParams.segment' ..
+%       [ segment_integer segment_side], where segment_integer denotes the
+%       number of the segment and segment_side encodes the start (0) or end
+%       of the segment (1) as 0 or 1.
+%
+%	NOTES: I'm expecting this struct will be expanded over time to encopass
+%	greater and greater levels of selection criteria. I think this function
+%	could beecome useful to future analyses, if we make it robust and
+%	expandable.
+%
+% OUTPUTS
+% ---------------------
+% times ... all times that fall within sampleParams criteria
+
+times = data.linpos.statematrix.time;
+sample = ones(size(times));				% Sample, a logical vector describing 
+										% time entries that belong in our sample .. 
+										% Each section of code below, a
+										% constraint test is performed, and
+										% the sample is reduced by 0'ing
+										% irrelevant time points and 1'ing
+										% relevant points.
+
+%% Subset out the trajectory
+
+if(isfield(sampleParams,'trajbound_type'))
+	
+	% Grab a subset of times corresponding to the starts and stops for the
+	% particular trajectory type
+	subset_trajbound_indices = find(sampleParams.trajbound_type ...
+		== data.trajinfo.trajbound);	% subsetting
+	trajbound_startStops = ...
+		data.trajinfo.trajtime(subset_trajbound_indices,:);	% start-stop times for subset
+	
+	% For loop over each start-stop time pair, and acquire a logical vector
+	% describing which elements are in the pair of times. Use logical OR to
+	% gradually create a picture of all times to be investigated.
+	
+	logical_start_stop	= zeros(size(times));	% detects points in a single trajectory per loop iteration
+	all_times			= zeros(size(times));	% updates to catalogue all points per iteration
+	
+	for i = 1:size(trajbound_startStops,1)
+		
+		% Find which times in the (start, stop) boundary
+		logical_start_stop = ( times > trajbound_startStops(i,1) ) & ...
+			( times < trajbound_startStops(i,2) );
+		
+		% Add the points found for ith trajectory to total record of times
+		all_times = all_times | logical_start_stop;
+		
+	end
+	
+	% Update the sample
+	sample = sample & all_times;
+	
+	% Remove variables just used from namespace
+	clear logical_start_stop all_times subset_trajbound_indicies ...
+		trajbound_startStops;
+	
+end
+
+
+%% Subset out radius around points of interest
+% This is the section where, if the user inputs points and radii to sample
+% around, we move through each point and find the times that fall inside
+% circumscribed regions
+
+% Subset out circle of data IF user has provided the field. Do not
+% circumscribe if the user has not provided it.
+if(isfield(sampleParams, 'circleParams'))
+    
+    % If the user specifies the point to sample in the parameters, use
+    % that, otherwise, use other criteria. For now, this other criteria can
+    % only be segment.
+    if( ~isfield(sampleParams.circleParams, 'center') )
+        if(isfield(sampleParams.circleParams, 'segment'))
+            % User has given a segment input, so we will extract it from
+            % the data
+            
+            % Simplify variable name we will use, for readability
+            segmentCoords = data.linpos.segmentInfo.segmentCoords;
+			segment = sampleParams.circleParams.segment;
+			
+            % Acquire the x coordinate of the segment
+            sampleParams.circleParams.center(1) = ...
+                segmentCoords(segment(1), segment(2)*2 + 1);
+            % Acquire the y coordinate of the segment
+            sampleParams.circleParams.center(2) = ...
+                segmentCoords(segment(1), segment(2)*2 + 2);
+            
+        end
+    end
+	
+	% feed animal (x,y) position list and selection parameters
+	circ_subset_indices = circumscribePoint( data.pos.data(:,2:3), ...
+		sampleParams.circleParams);
+	
+	% WE have indicies that belong, but we need a logical vector
+	circ_logical = zeros(size(times));
+	circ_logical(circ_subset_indices) = ...
+		~circ_logical(circ_subset_indices);
+	
+	% Adjust the sample
+	sample = sample & circ_logical;
+	
+end
+
+%% Return total sample
+%i.e. correct times from sample logical
+times = times(sample);
+
+%% HELPER FUNCTIONS
+
+function [ indices ] = circumscribePoint( trajectoryData, circumParms )
+%CIRCLESAMPLE returns indices for a circle of points around a position
+%   circleSample obtains indices of a circle of points around a position
+%   provided by the user.
+%
+% ------ INPUTS
+%
+% 'trajectoryData',
+%	matrix containing fields detailing the trajectory structure to find
+%	the proper indices on ... trajectoryData is a time * 2 matrix, where
+%	the first column is x and the second column is y coordinate.
+%
+% 'circumParams'
+%	structure containing fields detaling the parameters controlling the
+%	sampling process.
+%
+%	'circumParams.radius' controls the sampling radius
+%	'circumParams.center' = 1x2 [x y] coordinate of the point to draw the
+%	radius around and sample from.
+%
+% ------ OUTPUTS
+%
+% indices
+%	contains the indices that describe points in the trajectory data that
+%	are inside the circle specified by the circumParams
+
+
+%% Pre-processing phase .. simplify variable names
+xc = circumParms.center(1); yc = circumParms.center(2);
+
+x_pos = trajectoryData(:,1); y_pos = trajectoryData(:,2);
+
+%% Processing, pulling points
+
+distance_from_center = sqrt(( x_pos - xc ).^2 + (y_pos - yc).^2);
+
+indices = find(distance_from_center < circumParms.radius);
+
+
+end
+
+end
---
+++ gatherWindowsOfData.m
@@ -0,0 +1,301 @@
+function [acquisition] = ...
+	gatherWindowsOfData(dataFolder,dataToGet)
+% function gatherWindowsOfData
+% for gathering all data for an (animal/prefix epoch day tet) tuple or set of
+% tuples. This function will be used to gather up all of the eeg data
+% around a point of interest for analysis. The point of interest comes from
+% calling a function that detects transitions from segment x to segment y
+% on the maze.
+%
+% INPUTS -----------
+%
+% dataFolder - the folder containing the relevant piece of data we'd like
+% to process
+% 
+% dataToGet - type = struct
+%   This data type is a struct whose fields contain all of the relevant
+%   information about which animals, which days, which epochs, and which
+%   tetrodes to obtain transition point data for
+%
+%   --- fields of dataToGet ---
+% 
+%	'.animals' .. this field is a struct. Each animal to be analyzed gets
+%	it's own field. e.g.
+%	
+%		.animals.HPa
+%		.animals.HPb
+%
+%	Each animal field contains fields describing a list of
+%	days, epochs, and tetrodes to acquire for that animal. Reason I've
+%	chosen this way is to allow painless multi-animal analysis, by a single
+%	function. No for-looping over animals in the Main function calling it.
+%	Animals are allowed to each have their own sets of days, epochs,
+%	tetrodes.
+%   
+%   '.datType', .. refers to the data type in the prefix of the file we're
+%   to load, be it eeg, theta, thetagnd, et cetera. So e.g.,
+%
+%		.datType = 'eeg' ", or
+%		.datType = 'thetagnd' 
+%
+%	This parameter selects chich type of data to window.
+
+%
+%   '.sampleParams' .. struct that describes the characteristics of the
+%   time points to sample. This structure will include any information
+%   about the maze or behavior we might need to gather windows of data.
+%   It's a struct giving it the ability to flex to fit new data types in
+%   the future if we chose to window around different parameters. It's
+%   designed to be robust/extensible, and pretty much just sends this
+%   struct straight into acquireSample function to obtain times. In this
+%   alpha version, acquireSample can select a times on an entire segment,
+%   or on a subset of a segment. Times can also be selected for a
+%   trajectory class (inbound or outbound).
+%
+%		'sampleParams.trajbound_type' = 0 or 1 .. if 0, filter times that
+%		are outboud. If 1, filter times that are inbound.
+%
+%		'sampleParams.circleParams' .. structure that contains data for
+%		filtering times circumscribed by a position.
+%
+%			'sampleParams.circleParams.radius' .. the radius in pixels to
+%			detect around the point.
+%
+%			'sampleParams.circleParams.center' .. specifies [x y] to sample
+%			around. Alternatively, one can specify the segment and the
+%			method will find the [x y] by filling the .segmentCoordinate
+%			field below.
+%
+%			'sampleParams.circleParams.segment' .. [ segment_integer
+%			segment_side], where segment_integer denotes the number of the
+%			segment and segment_side encodes the start (0) or end of the
+%			segment (1) as 0 or 1.
+%   -----------------------------
+%
+% OUTPUTS ----------
+%  
+% "acquisition" array such that:
+% 
+% acquisition(i).animal = animal name
+% acquisition(i).data{day,epoch,tetrode} = [] matrix such that each column
+% contains a copy of the data with only relevant values for a detected
+% trigger time. There are as many columns as there were triggered events.
+% 
+
+
+%% FUNCTION CONSTANTS ( .. these will later become parameters ..)
+WINDOW = [-0.020 0.030];        % Seconds before and after the transition
+
+
+%% Preprocessing Steps
+
+% record initial folder so we can get back to it
+initial_folder = pwd;
+
+% change directory into the file address containing the data to acquire
+cd(dataFolder);
+path(path,genpath(dataFolder)); % all all subfolders to path so we'll definitely be able to detect the data
+
+
+%% Processing Loop
+
+try % try-catch block in case data is not found, returns to calling folder and THEN (and only then) throws an error .. the folder return would not occur if we allowed matlab to handle the error.
+	
+% Prepare list of animals to process, creates a cell of them from the
+% fields of the animal field in the dataToGet structure.
+animal_list = fields(dataToGet.animals);
+
+% Prepare struct, generates one set of windowed data per animal, with data
+% indexed by {day, epoch, tetrode}
+acquisition.animal = []; % strores animal name
+acquisition.data = {};	 % stores a matrix in each {day, epoch, tetrode}
+
+for a = 1:numel(animal_list)
+	
+	% Assign animal name
+	anim = animal_list{a};
+	acquisition(a).animal = anim;
+	
+	% Preallocate cell
+	acquisition(a).data = cell(...
+		numel(dataToGet.animals.(anim).days), ...
+		numel(dataToGet.animals.(anim).epochs), ...
+		numel(dataToGet.animals.(anim).tetrodes));
+	
+    for d = dataToGet.animals.(anim).days
+        
+        % Preprocess the day string to add a 0 before the number if it's less
+        % than 10
+        if(d < 10)
+            day = num2str(['0' num2str(d)]);
+        else
+            day = num2str(d);
+        end
+		
+        for e = dataToGet.animals.(anim).epochs
+			
+			%% Acquire run data for (animal, day, epoch)
+            
+			% Generate the file string from each of the cell elements passed from
+			% the nested loop over cells
+			file_cell{1} = [anim 'linpos' day ];
+			file_cell{2} = [anim 'trajinfo' day ];
+			file_cell{3} = [anim 'pos' day ];
+			
+			% load it all up
+			for i = 1:numel(file_cell)
+				load(file_cell{i});
+			end
+			
+			% Get epoch of linearized position data!
+			data.linpos = linpos{d}{e};
+			% Get epoch of trajinfo data!
+			data.trajinfo = trajinfo{d}{e};
+			% Get epoch of raw position data!
+			data.pos = pos{d}{e};
+
+			%% Find times to sample
+			
+			% Find times!!
+			timesInSample = acquireSample( data, ...
+				dataToGet.sampleParams);
+			allTimes = data.pos.data(:,1);
+			% Get list of continuous time windows - we pass in sample times
+			% and well as a list of all times
+			windowTimes = ...
+				generateContiguousSamples(timesInSample, allTimes);
+			
+			%% Window out the correct data per tetrode
+            for t = dataToGet.animals.(anim).tetrodes
+                
+				% Acquire matrix of windowed data
+                acquisition(a).data{d,e,t} ...
+					= windowData(dataToGet.datType, ...
+					anim,d,e,t,...
+					windowTimes);
+                
+			end % of tetrode loop
+		end % of epoch loop
+	end % of day loop
+end % of animal loop
+
+% Catching errors
+catch ME
+% Decommision the folders we added into the path at the start of the
+% function.
+disp('ERROR IN gatherWindowOfData...');
+disp('Throwing exception data...');
+rmpath(genpath(dataFolder));
+cd(initial_folder);
+throw(ME);
+
+end
+
+%% Helper functions
+% --- Helper function: processTuple
+function winData = windowData(dat, anim, day, epo, tet, windowTimes)
+    
+    % Preprocess the day string to add a 0 before the number if it's less
+    % than 10
+    if(day < 10)
+        day_str = ['0' num2str(day)];
+    else
+        day_str = num2str(day);
+	end
+	
+	if(tet < 10)
+		tet_str = ['0' num2str(tet)];
+	else
+		tet_str = num2str(tet);
+	end
+
+    %% Acquire LFP or Spike data at these detected times
+    file_string = [ anim dat day_str '-' num2str(epo) '-' tet_str ];
+    data_import = load(file_string);
+    
+    % Have to assign the variable that comes out of load, which is itself
+    % could be a variety of names to a general name
+    temp = [];
+    eval(['temp = data_import.' dat '{' num2str(day) '}' ...
+		'{' num2str(epo) '}']);
+	temp = temp{1};
+    
+    % Now we need to estimate the time corresponding to the points in the
+    % data
+    time_start = temp.starttime;
+    time_end = temp.starttime + (1/temp.samprate)*numel(temp.data(:,1));
+    
+    % Estimate time vector for the data set
+    time_vec = linspace(time_start, time_end, numel(temp.data(:,1)));		% TO IMPROVE : Need to allow user to input field of data, and use that to determine column number
+    
+    % Create 2D matrix that will hold a window of data per trigger time as
+    % detected by segment transitions
+    winData = zeros( size(windowTimes,1), numel(time_vec) );
+    
+    % Grab all of the data in the windows of time
+    for ind = 1:size(windowTimes,1)
+        
+        % Create logical vector for selecting the proper data to store
+        times_of_interest = (time_vec > windowTimes(ind,1)) & ...
+            (time_vec < windowTimes(ind,2));
+        
+        % Multiply by logical to zero out irrelevant data, and store vector
+        % into a column of the matrix.
+        winData(ind,:) = int16(times_of_interest').* temp.data(:,1);		% TO IMPROVE: Need to AUTOMATICALLY  cast times_of_interest to whatever the data's type is!
+
+    end
+    
+    % Return the data points
+    return;
+    
+end
+
+function [starts_stops] = generateContiguousSamples(sampleTimes, allTimes)
+% This function detects contiguous times in the sample times and outputs,
+% and re-casts the times into a list of starts and stops. Each row is a
+% start stop pair. In between each start stop pair are contiguous times.
+%
+% It's a beta version. This is not the best way to detect contnuity. Just a
+% fast version and easy to cook up. I'm expecting that we'll change this.
+
+% Get list of all indices of sample times
+[~, sampleIndices] = ismember(sampleTimes,allTimes);
+% Generate a diff of these indices .. contiguous will separate by 1 step
+diff_samp_ind = diff(sampleIndices);
+
+% Generate list of incontinuities in the diff
+points_of_incontinuity = find(diff_samp_ind > 3);
+
+
+% diff_times = diff(sampleTimes);
+% sorted_diff_of_times = sort(diff_times);
+% longest_time_threshold = diff_times( round(end * 0.995 ) ) * 4; % take the upper 0.5% percentile of values, and multiply by 4
+% 
+% points_of_incontinuity = find(diff_times > longest_time_threshold);
+
+% Create a matrix of	[start_1 stop_2]
+%						[start_2 stop_2]
+%						[ ...		   ]
+%						[start_n stop_n]
+%
+diff_starts_stops(1,:) = points_of_incontinuity; 
+diff_starts_stops(2,:) = points_of_incontinuity+1;
+contiguous_regions = zeros(2, size(diff_starts_stops,2) + 1);
+contiguous_regions(:,1:end-1) = diff_starts_stops;
+contiguous_regions(2:end-1) = contiguous_regions(1:end-2);
+contiguous_regions(1) = 1;
+contiguous_regions(end) = numel(allTimes);
+contiguous_regions = contiguous_regions';
+
+% Convert start and stop vector indicies in contiguous regions into times
+starts_stops = allTimes(contiguous_regions);
+
+end
+
+%% Post-processing phase
+% return to calling folder and remove dataFolder tree from the path
+rmpath(genpath(dataFolder));
+cd(initial_folder);
+
+end
+
