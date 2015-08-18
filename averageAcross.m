72ac7fc506b217cc8154d2c263de9fe1ad36150f
mczielinski <mczielinski@gmail.com>
2015-08-18 17:28:28 -0400

    Cleaned up averageAcross.m. Added some comments, cleared our redudntant loop. Still needs work. Add other averaging functions, other fields for returned struct.

--- averageAcross.m
+++ averageAcross.m
@@ -2,41 +2,53 @@ function [avg_specgram] = averageAcross(specgrams, sets)
 % Function controls the averaging across spectrograms.
 
 % if flag== trials, Output = Output(animals).{day,epoch,tetrode}
+sets='trials';
+specgrams=Output;
+animNum= length(specgrams);
 
-animNum= length(Output);
-for i= 1:animNum;
-    datSize(i,:)= size(Output(i).output);
-end
+for a= 1:animNum;
+    datSize(a,:)= size(specgrams(a).output);
 
-relevElem=find( ~cellfun(@isempty,Output(i).output));
-for el= 1:length(relevElem)
-[day(el), epc(el), tet(el), tri(el)]= ind2sub(datSize(i,:),relevElem(el));
-end
+% Get nonzero input array indices
+relevElem=find( ~cellfun(@isempty,specgrams(a).output));
 
-idx=[day; epc; tet; tri]';
+% convert indices to 4D indices
+[day, epc, tet, tri]=ind2sub(datSize(a,:),relevElem);
 
-uDay= unique(day);
-uEpc= unique(epc);
-uTet= unique(tet);
-uTri= unique(tri);
+% assemble into one mat
+idx= [day epc tet tri];
 
-if sets == 'trials'
-   for a=1:animNum;
-        for d=1:length(uDay); 
-            for e= 1:length(uEpc);
-                for t=1:length(uTet);
-                    idxs= idx((idx(:,1)== uDay(d) & idx(:,2)== uEpc(e) & idx(:,3)== uTet(t) ),:);
-                    for r=1:length(idxs);
-                               tempS(r,:,:)= Output(i).output{uDay(d), uEpc(e), uTet(t), r}.S;
-                        tempSerror(r,:,:,:)= Output(i).output{uDay(d), uEpc(e), uTet(t), r}.Serror;
-                    end 
-                    avg_specgram(a).meanS{     uDay(d), uEpc(e), uTet(t)} = squeeze(mean(tempS,1));
-                    avg_specgram(a).meanSerror{uDay(d), uEpc(e), uTet(t)} = squeeze(mean(tempSerror,1));
+% get unique values from each dimension where data is stored
+uDay= unique(day); uEpc= unique(epc);
+uTet= unique(tet); uTri= unique(tri);
 
+if sets == 'trials';
+    for d= 1:length(uDay);
+        for e= 1:length(uEpc);
+            for t= 1:length(uTet);
+                
+                % get all trials and locations
+                idxs= idx((idx(:,1)== uDay(d) &...
+                           idx(:,2)== uEpc(e) &...
+                           idx(:,3)== uTet(t) ),:);
+                
+                for r= 1:length(idxs);
+                    % store all gathered S'es and Serrors in temp matrices
+                    tempS(r,:,:)=...
+                    specgrams(a).output{uDay(d), uEpc(e), uTet(t), r}.S;
+                    tempSerror(r,:,:,:)=...
+                    specgrams(a).output{uDay(d), uEpc(e), uTet(t), r}.Serror;
                 end
+                
+                % package mean Specs and SpecErrors
+                avg_specgram(a).output{uDay(d), uEpc(e), uTet(t)}....
+                meanS     = squeeze(mean(tempS     ,1));
+                avg_specgram(a).output{uDay(d), uEpc(e), uTet(t)}...
+                meanSerror= squeeze(mean(tempSerror,1));
+                
             end
-        end 
-   end
-    
-
+        end
+    end
 end
+end
+
