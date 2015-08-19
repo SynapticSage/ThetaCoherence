40c91404f5fa13be8fb03a19dc18fcead8913caa
mczielinski <mczielinski@gmail.com>
2015-08-19 00:30:04 -0400

    Slight Modification
    Subtracted extraneous line. Clearer comments. Added params field for future modification. Setting up auto dim detection. To do: undo hard coding of dimensions and dimension-dependent variables.

--- averageAcross.m
+++ averageAcross.m
@@ -1,18 +1,20 @@
-function [avg_specgram] = averageAcross(specgrams, sets)
+function [avg_specgram] = averageAcross(specgrams, sets, params)
 % Function controls the averaging across spectrograms.
 
 % if flag== trials, Output = Output(animals).{day,epoch,tetrode}
 sets='trials';
-specgrams=Output;
 animNum= length(specgrams);
 
 for a= 1:animNum;
-    datSize(a,:)= size(specgrams(a).output);
 
-% Get nonzero input array indices
+% retrieve general info on array size and # of dims 
+datSize(a,:)= size(specgrams(a).output);
+datDims(a,:)= ndims(specgrams(a).output);
+
+% Get nonzero input array indices (linear indexing)
 relevElem=find( ~cellfun(@isempty,specgrams(a).output));
 
-% convert indices to 4D indices
+% convert linear indices to subscript (dimensional) indices
 [day, epc, tet, tri]=ind2sub(datSize(a,:),relevElem);
 
 % assemble into one mat
@@ -20,14 +22,14 @@ idx= [day epc tet tri];
 
 % get unique values from each dimension where data is stored
 uDay= unique(day); uEpc= unique(epc);
-uTet= unique(tet); uTri= unique(tri);
+uTet= unique(tet); %uTri= unique(tri);
 
 if sets == 'trials';
     for d= 1:length(uDay);
         for e= 1:length(uEpc);
             for t= 1:length(uTet);
                 
-                % get all trials and locations
+                % get the indices for all relevant inputs for this operation (avg across trials)
                 idxs= idx((idx(:,1)== uDay(d) &...
                            idx(:,2)== uEpc(e) &...
                            idx(:,3)== uTet(t) ),:);
@@ -40,7 +42,7 @@ if sets == 'trials';
                     specgrams(a).output{uDay(d), uEpc(e), uTet(t), r}.Serror;
                 end
                 
-                % package mean Specs and SpecErrors
+                % package mean Specs and SpecErrors, squeeze out singleton
                 avg_specgram(a).output{uDay(d), uEpc(e), uTet(t)}...
                 .meanS     =squeeze(mean(tempS     ,1));
                 avg_specgram(a).output{uDay(d), uEpc(e), uTet(t)}...
