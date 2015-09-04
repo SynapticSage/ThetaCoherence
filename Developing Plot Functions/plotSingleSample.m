4ed3a71c87e85c9baf5484bab2d4f2b76cd693b1
Ryan <neuroryoung@gmail.com>
2015-09-04 16:25:33 -0400

    Updated plotSingleSample to show centroid of sample

--- Developing Plot Functions/plotSingleSample.m
+++ Developing Plot Functions/plotSingleSample.m
@@ -1,18 +1,17 @@
 function plotSingleSample(pos,ssi,i)
-
-    if isstruct(pos) 
-        data = pos.data;
-        init = ssi(i,1);
-        fin = ssi(i,2);
-    elseif iscell(pos)
-        if pos{2}<10; ds=['0' num2str(pos{2})]; else ds=['0' num2str(pos{2})];end
-        temp = load([pos{1} 'pos' ds])
-        data=temp.pos{pos{2}}{pos{3}}.data;
-    end
+    
+    data = pos.data;
+    init = ssi(i,1);
+    fin = ssi(i,2);
+    center = round((ssi(i,2)-ssi(i,1))/2) + ssi(i,1);
     
     plot( data(:,2),        data(:,3)       );
     hold on;
     plot( data(init:fin,2), data(init:fin,3),'*-');
+     plot( data(center,2), data(center,3),'o','LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor',[0.5,0.5,0.5]);
     hold off;
 
 end
